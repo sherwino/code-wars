4e670a38fd6c48f1d5222b3a67cdbdb40b0b9bb7
Sherwino <sherwination@gmail.com>
2017-08-31 08:39:12 -0400

    added new problem (unsolved)

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # code-wars
-Keeping a log of all of my codewars.com responses. 
+Keeping a log of all of my codewars.com responses.
 To keep track of some of the progress I have made over time.
 
 # JavaScript
@@ -53,4 +53,30 @@ Test.expect(!isValidWalk(['w','e','w','e','w','e','w','e','w','e','w','e']), 'sh
 Test.expect(!isValidWalk(['w']), 'should return false');
 Test.expect(!isValidWalk(['n','n','n','s','n','s','n','s','n','s']), 'should return false');
 
+```
+
+
+4. EXTRA CREDIT - this is a sample problem from [codility.com]
+
+
+A binary gap within a positive integer N is any maximal sequence of consecutive zeros that is surrounded by ones at both ends in the binary representation of N.
+
+For example, number 9 has binary representation 1001 and contains a binary gap of length 2. The number 529 has binary representation 1000010001 and contains two binary gaps: one of length 4 and one of length 3. The number 20 has binary representation 10100 and contains one binary gap of length 1. The number 15 has binary representation 1111 and has no binary gaps.
+
+### Write a function:
+```
+    int solution(int N);
+```
+that, given a positive integer N, returns the length of its longest binary gap. The function should return 0 if N doesn't contain a binary gap.
+
+For example, given N = 1041 the function should return 5, because N has binary representation 10000010001 and so its longest binary gap is of length 5.
+
+### Assume that:
+```
+        N is an integer within the range [1..2,147,483,647].
+```
+### Complexity:
+```
+        expected worst-case time complexity is O(log(N));
+        expected worst-case space complexity is O(1).
 ```
--- javascript.js
+++ javascript.js
@@ -47,11 +47,11 @@ function GetSum2(a,b)
 function songDecoder(song) {
   //create a new array by splitting string every time wub appears
   let splitSong = song.split("WUB");
-  
+
   //iterate over array
   // then create a new array with results of filter
   splitSong = splitSong.filter(item => item !== "");
-  
+
   //then create a new string with spaces in between each item
   let songString = splitSong.join(" ");
   return songString;
@@ -74,59 +74,59 @@ function songDecoder2(song){
 // 3--------------------------------------------
 
 function isValidWalk(walk) {
-  
+
     //insert brilliant code here
     let x = 0, y = 0;
     let coordinate = [];
     if (walk.length === 10) {
       walk.forEach((dir) => {
         switch(dir.toLowerCase()) {
-        
+
           case "n":
-            
+
             coordinate = [x, y+=1];
             break;
-            
+
           case "s":
             coordinate = [x, y-=1];
             break;
-            
+
           case "e":
             coordinate = [x+=1, y];
             break;
-          
+
           case "w":
             coordinate = [x-=1, y];
             break;
-            
+
           } //end of switch
         });//end of forEach
        } else {
        return false;
     }; //end of if statement
-  
+
     if(x == 0 && y == 0);
-  
+
   };
-  
+
   // better solutions
   function isValidWalk1(walk) {
     var dx = 0
     var dy = 0
     var dt = walk.length
-    
+
     for (var i = 0; i < walk.length; i++) {
       switch (walk[i]) {
-        case 'n': dy--; break
-        case 's': dy++; break
-        case 'w': dx--; break
-        case 'e': dx++; break
+        case 'n': dy--; break;
+        case 's': dy++; break;
+        case 'w': dx--; break;
+        case 'e': dx++; break;
       }
     }
-    
+
     return dt === 10 && dx === 0 && dy === 0
   }
-  
+
   //better solutions two
   function isValidWalk2(walk) {
     function count(val) {
@@ -134,16 +134,79 @@ function isValidWalk(walk) {
     }
     return walk.length==10 && count('n')==count('s') && count('w')==count('e');
   }
-  
-  
+
+
   //better solution three
   function isValidWalk3(walk) {
     const north = walk.filter(item => { return item === "n" }).length;
     const south = walk.filter(item => { return item === "s" }).length;
     const east = walk.filter(item => { return item === "e" }).length;
     const west = walk.filter(item => { return item === "w" }).length;
-    
+
     return walk.length === 10 && north === south && east === west;
   }
 
 // 3--------------------------------------------
+
+
+// 4 ----------EXTRA CREDIT --------------------
+function solution(N) {
+
+    let gapStart, gapEnd, gapsFound;
+
+    checkforGaps(N);
+    checkforZeros(N);
+
+} //end of solution fn
+
+solution(1041);
+// ====> 5
+
+  function checkforGaps(num) {
+      let bi = num.toString(2);
+      gapsFound = [];
+
+      for (i=0; i < bi.length; i++) {
+
+        if (bi[i] === "1" && bi[i+1] === "0") {
+          gapStart = i;
+          console.log("Gap Start at idx: " + gapStart);
+
+        } else if (bi[i] === "0" && bi[i+1] === "1") {
+            gapEnd = i+1;
+            gapsFound.push(gapStart, gapEnd);
+            console.log("Gap End at idx: " + gapEnd);
+            console.log("Gaps found " + gapsFound);
+
+        } //end of if statements
+
+      } //end of for loop
+
+    } //end of checkforGaps fn
+
+
+  function checkforZeros(num) {
+    let bi = num.toString(2);
+    let zeroCnt, zeroArray;
+
+    gapsFound.forEach((ele, idx) => {
+      if ( idx % 2 ) {
+        gapStart = ele;
+        console.log("Start " + gapStart);
+      } else {
+        gapEnd = ele;
+        console.log("End " + gapEnd);
+      }
+    });
+
+    for (i=gapStart; i < gapEnd; i++) {
+
+      if (bi[i] === "0") {
+        zeroCnt+=1;
+        console.log("Zeros counted " + zeroCnt);
+        zeroArray.push(zeroCnt);
+      }
+    }
+    // gapsFound.push(zeroCnt);
+    // return Math.max(...gapsFound);
+    }
